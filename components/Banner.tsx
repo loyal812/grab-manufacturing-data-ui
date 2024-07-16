aad1e3be21ecc52f937845103745afe81cd5a36d
loyal812 <jh.chan0812@gmail.com>
2024-07-16 01:17:44 -0400

    fix: banner component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,16 +7,14 @@ const Banner = () => {
   };
   return (
     <div className="absolute inset-0 max-h-screen ">
-      <Image
-        src="https://images.unsplash.com/photo-1613690399151-65ea69478674?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
-        layout="fill"
-        className="object-cover"
-        alt="banner image"
+      <video
+        src="https://cdn.dribbble.com/users/1542362/screenshots/16499285/media/452718516761091228a754666099e367.mp4"
+        autoPlay
+        muted
+        loop
+        className="absolute inset-0 h-full w-full object-fill"
       />
-      <div
-        className="absolute inset-0 bg-gray-900 opacity-60 mix-blend-multiply"
-        aria-hidden="true"
-      />
+
       <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8 text-center ">
         <h1 className="text-4xl font-poppins tracking-tight mt-32 sm:mt-2  text-white sm:text-5xl lg:text-6xl">
           ComplianceGrabber 2.0
@@ -30,7 +28,7 @@ const Banner = () => {
       </div>
       <div className="absolute bottom-24 inset-x-auto w-full mt-56 sm:mt-36 flex justify-center px-2">
         <button
-          className="font-poppins relative overflow-hidden text-xl group text-white rounded-full inline-flex items-center justify-center space-x-4  py-2 border-4 border-white  min-w-[350px] px-4"
+          className="font-poppins relative overflow-hidden text-lg group text-white rounded-full inline-flex items-center justify-center space-x-4  py-1 border-4 border-white  min-w-[350px] px-4"
           onClick={scrollDown}
         >
           <p className="lg:z-10 ">GET STARTED</p>
