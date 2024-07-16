ec1c9ac3de7dd881c6b5537c128f9d7d144d9462
loyal812 <jh.chan0812@gmail.com>
2024-07-16 01:16:05 -0400

    fix: combo box element

--- components/Combobox.tsx
+++ components/Combobox.tsx
@@ -27,8 +27,11 @@ function Dropdown({
       : options.filter((option) => {
           return option.toLowerCase().includes(query.toLowerCase());
         });
+  const handleSelectedMember = (option: string) => {
+    setSelectedoption ? setSelectedoption(option) : null;
+  };
   return (
-    <Combobox as="div" value={selectedoption} onChange={setSelectedoption}>
+    <Combobox as="div" value={selectedoption} onChange={handleSelectedMember}>
       <Combobox.Label className="block text-md font-poppins text-gray-700">
         {title}
       </Combobox.Label>
@@ -36,7 +39,6 @@ function Dropdown({
         <Combobox.Input
           className=" w-full rounded-md border border-gray-300 bg-white py-4 ml-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
           onChange={(event) => setQuery(event.target.value)}
-          displayValue={(option) => option}
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon
@@ -60,12 +62,7 @@ function Dropdown({
               >
                 {({ active, selected }) => (
                   <>
-                    <span
-                      className={classNames(
-                        "block truncate",
-                        selected && "font-semibold"
-                      )}
-                    >
+                    <span className={classNames("block truncate")}>
                       {option}
                     </span>
 
