d70209d231b9ec8fb8a7e1c6c818e652fc9337f7
loyal812 <jh.chan0812@gmail.com>
2024-08-09 00:36:15 -0400

    style: banner and swiper component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -10,7 +10,7 @@ const Banner = () => {
   return (
     <div className="absolute inset-0 max-h-screen bg-blue-900 ">
       <video
-        src="/animation.mp4"
+        src="/lastanimation.mp4"
         autoPlay
         muted
         loop
@@ -18,46 +18,42 @@ const Banner = () => {
       />
 
       <motion.div
-        className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8 text-center "
+        className="relative max-w-7xl mx-auto  pt-16 pb-24 px-4 sm:py-32 sm:px-6 lg:px-8 text-center "
         initial={{ opacity: 1, y: "100vh" }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
       >
-        <h1 className="text-4xl font-poppins tracking-tight mt-32 sm:mt-2  text-white sm:text-5xl lg:text-6xl">
+        <h1 className="text-4xl font-poppins tracking-tight mt-16 sm:mt-2  text-white sm:text-5xl lg:text-6xl pb-4">
           ComplianceGrabber 2.0
         </h1>
-        <p className="mt-6 hidden sm:block text-xl text-white font-poppins text-center leading-10">
+        {/* <p className="mt-6 hidden sm:block text-xl text-white font-poppins text-center leading-10">
           You’re digital tool to get all you need in one place and from all the
           best suppliers,we strive to make all the product details available for
           you,coming from the most used and trusted suppliers all around the
           Globe !
-        </p>
+        </p> */}
       </motion.div>
-      <div className="absolute bottom-16 inset-x-auto w-full mt-[18rem] sm:mt-36 flex justify-center px-2 ">
-        <motion.button
-          initial={{ opacity: 0, x: "-100vh" }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.6, duration: 0.4 }}
-          className="font-poppins rounded-l-full  relative  mx-2 overflow-hidden text-lg group text-white rounded-md inline-flex items-center justify-center space-x-4  py-3 border-4 border-white  min-w-[350px] px-4"
-          onClick={scrollDown}
-        >
-          <p className="lg:z-10 ">GO DASHBORD</p>
-
-          <div className=" hidden lg:block smooth-transition w-[250px] h-[100px] group-hover:translate-x-[175px]  absolute -left-[266px]" />
-          <div className=" hidden lg:block w-[250px] smooth-transition h-[100px] group-hover:-translate-x-[175px] absolute  -right-[250px]" />
-        </motion.button>
-        <motion.button
-          initial={{ opacity: 0, x: "-100vh" }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.9, duration: 0.4 }}
-          className="font-poppins rounded-r-full bg-gradient-to-r from-indigo-700 via-blue-500 to-blue-700 relative mx-2  overflow-hidden text-lg group text-white rounded-md inline-flex items-center justify-center space-x-4  py-3 border-4 border-white  min-w-[350px] px-4"
-          onClick={scrollDown}
-        >
-          <p className="lg:z-10 ">GET STARTED</p>
-
-          <div className="  hidden lg:block smooth-transition w-[250px] h-[100px] group-hover:translate-x-[175px]  absolute -left-[266px]" />
-          <div className=" hidden lg:block w-[250px] smooth-transition h-[100px] group-hover:-translate-x-[175px] absolute  -right-[250px]" />
-        </motion.button>
+      <div className="absolute bottom-5 inset-x-auto w-full mt-[18rem] sm:mt-36 flex justify-center px-2 ">
+        <div className="flex  space-x-2 ">
+          <motion.button
+            initial={{ opacity: 0, x: "-100vh" }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.6, duration: 0.4 }}
+            className="font-poppins  relative  mx-2 overflow-hidden shadow-sm shadow-green-600 hover:bg-gray-400  smooth-transition text-lg group text-white rounded-md inline-flex items-center justify-center space-x-4  py-3 border border-white  min-w-[350px] px-6"
+            onClick={scrollDown}
+          >
+            <p className="lg:z-10 ">LOGIN</p>
+          </motion.button>
+          <motion.button
+            initial={{ opacity: 0, x: "-100vh" }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.9, duration: 0.4 }}
+            className="font-poppins  relative mx-2  overflow-hidden shadow-sm shadow-green-600 hover:bg-gray-400  smooth-transition text-lg group text-white rounded-md inline-flex items-center justify-center space-x-4  py-3 border border-white  min-w-[350px] px-6"
+            onClick={scrollDown}
+          >
+            <p className="lg:z-10 ">GET STARTED</p>
+          </motion.button>
+        </div>
       </div>
     </div>
   );
